9b6b8caa150d05de7bbfa74a02aa72221eda353d
Allendale Nato <anato2@academic.rrc.ca>
2025-08-04 16:48:36 -0500

    Implement tic-tac-toe functionality.

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import App from "./App";
+import {default as App, Square} from "./App";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
