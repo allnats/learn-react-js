aa6517aebdf8e45b2a59c7bd10e94cfc7efcb104
Allendale Nato <anato2@academic.rrc.ca>
2025-08-07 10:36:28 -0500

    Refactor pizza app.

---
+++ 03-pizza/src/pizzaApp.js
@@ -0,0 +1,66 @@
+import { pizzaData } from "./data.js";
+
+export default function pizzaApp() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <div className="menu">
+      <h2>Our menu</h2>
+      {pizzaData.map((pizza) => {
+        return (
+          <Pizza
+            name={pizza.name}
+            ingredients={pizza.ingredients}
+            price={pizza.price}
+            photoName={pizza.photoName}
+            soldOut={pizza.soldOut}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+function Pizza({ name, ingredients, price, photoName, soldOut }) {
+  return (
+    <div className="pizza">
+      <img src={photoName} alt={name} />
+      <div>
+        <h3>{name}</h3>
+        <p>{ingredients}</p>
+        <span>{price}</span>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  // return createElement("footer", null, "We are open till 8PM!")
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  const footerMsg = isOpen ? "We're currently open!" : "We're closed";
+
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}. {footerMsg}
+    </footer>
+  );
+}
