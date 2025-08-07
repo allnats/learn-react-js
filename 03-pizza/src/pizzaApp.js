51a52f752e6c13a8980362aaf33640b1452855b6
Allendale Nato <anato2@academic.rrc.ca>
2025-08-07 14:29:25 -0500

    Refactor pizza app's footer.

--- 03-pizza/src/pizzaApp.js
+++ 03-pizza/src/pizzaApp.js
@@ -19,29 +19,45 @@ function Header() {
 }
 
 function Menu() {
+  // Check if pizzaData is empty.
+  const isMenuEmpty = pizzaData.length === 0;
+
   return (
     <main className="menu">
       <h2>Our menu</h2>
 
       {/* Render the elements in "pizzaData" as Pizza components. */}
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => {
-          return <Pizza key={pizza.name} pizzaObj={pizza} />;
-        })}
-      </ul>
+      {!isMenuEmpty ? (
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone ove, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => {
+              return <Pizza key={pizza.name} pizzaObj={pizza} />;
+            })}
+          </ul>
+        </>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
+      )}
     </main>
   );
 }
 
 function Pizza({ pizzaObj }) {
   const { name, ingredients, price, photoName, soldOut } = pizzaObj;
+
+  // if (soldOut) return null;
+
   return (
-    <li className="pizza">
+    <li className={`pizza ${soldOut ? "sold-out" : null}`}>
       <img src={photoName} alt={name} />
       <div>
         <h3>{name}</h3>
         <p>{ingredients}</p>
-        <span>{price}</span>
+        <span>{soldOut ? "SOLD OUT" : price}</span>
       </div>
     </li>
   );
@@ -53,11 +69,24 @@ function Footer() {
   const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
-  const footerMsg = isOpen ? "We're currently open!" : "We're closed";
+  const footerMsg = isOpen
+    ? "We're open till 10:00 PM. Come visit us or Order online."
+    : `We're happy to welcome you between ${openHour}:00 and ${closeHour}:00`;
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. {footerMsg}
+      <Order isOpen={isOpen} footerMsg={footerMsg} />
     </footer>
+  );
+}
+
+function Order({ isOpen, footerMsg }) {
+  return (
+    <div className="order">
+      <p>
+        {new Date().toLocaleTimeString()}. {footerMsg}
+      </p>
+      {isOpen && <button className="btn">Order</button>}
+    </div>
   );
 }
