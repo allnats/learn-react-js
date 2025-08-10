c3b75aba6d28d8256df898203eb3e4d4e688c650
Allendale Nato <anato2@academic.rrc.ca>
2025-08-10 17:48:30 -0500

    Create travel-list app components.

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList listItems={initialItems} />
+      <Stats />
     </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏝️ Far Away 💼</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList({ listItems }) {
+  return (
+    <div className="list">
+      <ul>
+        {listItems.map((item) => {
+          return (
+            <li>
+              <input type="checkbox"></input>
+              <p>{item.description}</p>
+              <button>❌</button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on yourlist, and you already packed X%</em>
+    </footer>
   );
 }
 
