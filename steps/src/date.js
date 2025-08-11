970e63db4ff8f518de1b5a35d18d4b55dd337608
Allendale Nato <anato2@academic.rrc.ca>
2025-08-11 15:37:53 -0500

    Improve date schedule app.

--- steps/src/date.js
+++ steps/src/date.js
@@ -4,43 +4,57 @@ export default function DateChallenge() {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
 
-  function handleStepPlus() {
-    setStep((s) => s + 1);
-  }
-
-  function handleStepMinus() {
-    setStep((s) => s - 1);
-  }
-
-  function handleCountPlus() {
-    setCount((c) => c + step);
-  }
-
-  function handleCountMinus() {
-    setCount((c) => c - step);
-  }
-
   const currDate = new Date();
   const countDate = currDate.setDate(currDate.getDate() + count);
+
   return (
     <>
+      <input
+        type="range"
+        min={0}
+        max={10}
+        value={step}
+        onChange={(e) => setStep((c) => Number(e.target.value))}
+      ></input>
+      <span>{step}</span>
       <div>
-        <button onClick={handleStepMinus}>&#8722;</button>
-        <span>{`Step: ${step}`}</span>
-        <button onClick={handleStepPlus}>&#43;</button>
-      </div>
-      <div>
-        <button onClick={handleCountMinus}>&#8722;</button>
-        <span>{`Count: ${count}`}</span>
-        <button onClick={handleCountPlus}>&#43;</button>
+        <button
+          onClick={() => {
+            setCount((c) => c - step);
+          }}
+        >
+          &#8722;
+        </button>
+        <input
+          value={count}
+          onChange={(e) => {
+            setCount((c) => Number(e.target.value));
+          }}
+        ></input>
+        <button
+          onClick={() => {
+            setCount((c) => c + step);
+          }}
+        >
+          &#43;
+        </button>
       </div>
       <ShowDate dateString={currDate.toDateString()} count={count} />
+      <button
+        onClick={() => {
+          setStep((c) => 1);
+          setCount((c) => 0);
+        }}
+      >
+        Reset
+      </button>
     </>
   );
 }
 
 function ShowDate({ dateString, count }) {
-  if (count > 1) return <p>{`${count} days from today is ${dateString}`}</p>;
-  if (count < 0) return <p>{`${count} days ago today was ${dateString}`}</p>;
+  if (count > 0) return <p>{`${count} days from today is ${dateString}`}</p>;
+  if (count < 0)
+    return <p>{`${Math.abs(count)} days ago was ${dateString}`}</p>;
   return <p>{`Today is ${dateString}`}</p>;
 }
