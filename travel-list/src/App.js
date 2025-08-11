f6cdd83fba29821abb305bfc94b04030d1881943
Allendale Nato <anato2@academic.rrc.ca>
2025-08-10 19:17:50 -0500

    Add form states in travel-list app.

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 11, packed: true },
 ];
 
 function App() {
@@ -18,10 +21,41 @@ function Logo() {
   return <h1>🏝️ Far Away 💼</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleAddItem(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    // console.log(newItem);
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleAddItem}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button type="submit">Add</button>
+    </form>
   );
 }
 
@@ -30,16 +64,22 @@ function PackingList({ listItems }) {
     <div className="list">
       <ul>
         {listItems.map((item) => {
-          return (
-            <li>
-              <input type="checkbox"></input>
-              <p>{item.description}</p>
-              <button>❌</button>
-            </li>
-          );
+          return <Item item={item} key={item.id} />;
         })}
       </ul>
     </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <input type="checkbox"></input>
+      <p style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </p>
+      <button>❌</button>
+    </li>
   );
 }
 
