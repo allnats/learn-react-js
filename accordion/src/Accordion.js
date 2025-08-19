989e21a22e04ac3473de084296299f0f387fe8eb
Allendale Nato <anato2@academic.rrc.ca>
2025-08-18 21:09:51 -0500

    Refactor accordion app.

--- accordion/src/Accordion.js
+++ accordion/src/Accordion.js
@@ -1,37 +1,53 @@
 import { useState } from "react";
 
 export function Accordion({ faqList }) {
+  const [openAccordionID, setOpenAccordionID] = useState(null);
+
   return (
     <div className="accordion">
       {faqList.map((faq, idx) => (
         <AccordionItem
-          num={idx + 1}
           key={idx}
-          title={faq.title}
-          text={faq.text}
-        />
+          itemID={idx}
+          num={idx + 1}
+          faq={faq}
+          openID={openAccordionID}
+          onAccordionClick={setOpenAccordionID}
+        >
+          {faq.text}
+        </AccordionItem>
       ))}
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({
+  num,
+  itemID,
+  faq,
+  openID,
+  onAccordionClick,
+  children,
+}) {
+  const isOpen = itemID === openID;
 
-  function handleOnClick() {
-    setIsOpen((s) => !s);
+  function handleAccordionItemClick() {
+    onAccordionClick(isOpen ? null : itemID);
   }
 
   return (
-    <div className={`item ${isOpen && "open"}`} onClick={handleOnClick}>
+    <div
+      className={`item ${isOpen && "open"}`}
+      onClick={handleAccordionItemClick}
+    >
       <p className="number">{num}</p>
-      <p className="title text">{title}</p>
+      <p className="title text">{faq.title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
       <div
         className="content-box"
         style={isOpen ? { display: "block" } : { display: "none" }}
       >
-        {text}
+        {children}
       </div>
     </div>
   );
