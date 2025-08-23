5e19a655a0179e1f68fca227a04de47610b8e8e2
Allendale Nato <anato2@academic.rrc.ca>
2025-08-23 11:49:29 -0500

    Add static components in eat-n-split

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -20,5 +20,49 @@ const initialFriends = [
 ];
 
 export default function EatNSplit() {
-  return <div>Hello!</div>;
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance === 0 && (
+        <p className="">You and {friend.name} are even</p>
+      )}
+
+      <button className="button">Select</button>
+    </li>
+  );
 }
