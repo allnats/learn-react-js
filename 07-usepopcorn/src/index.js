f90aadad77afa2b89698ce9aeeeb8d9fd5b4b3c9
Allendale Nato <anato2@academic.rrc.ca>
2025-09-20 16:22:07 -0500

    Add StarRating component and update index.js to render it

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+// import "./index.css";
 import App from "./App";
+import StarRating from "./StarRating";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating />
   </StrictMode>
 );
