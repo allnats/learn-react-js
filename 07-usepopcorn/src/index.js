42e96134b6f121f55d35b117ced9df43ac5c9695
Allendale Nato <anato2@academic.rrc.ca>
2025-10-03 23:04:47 -0500

    feat: explore reusability with props

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -6,10 +6,13 @@ import StarRating from "./StarRating";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating maxRating={10} />
-    <StarRating />
-  </StrictMode>
+    <StrictMode>
+        {/* <App /> */}
+        <StarRating
+            maxRating={5}
+            messages={["Terrible", "Okay", "Good", "Great", "Amazing"]}
+        />
+        <StarRating maxRating={10} className="" />
+        <StarRating color="red" size={32} maxRating={20} />
+    </StrictMode>
 );
