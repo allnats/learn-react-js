1ff61a176e9e130efd6799f935edb8a5b8a8ee03
Allendale Nato <anato2@academic.rrc.ca>
2025-12-17 20:50:44 -0600

    feat: integrate json-server for quiz questions and implement data fetching in App component
    - Added json-server as a dependency to serve quiz questions from a local JSON file.
    - Created a new script in package.json to start the json-server.
    - Implemented useReducer for managing application state in App component.
    - Added useEffect to fetch questions from the json-server on component mount.
    - Created data/questions.json file containing quiz questions and options.

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,7 +1,47 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  question: [],
+  // Possible app states: loading, error, ready, active, and finished.
+  status: "loading",
+};
+
+// Main reducer function
+function reducer(state, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case "dataReceived":
+      return { ...state, question: payload, status: "ready" };
+    case "dataFetchError":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Invalid dispatch type");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // Effect that fetches the questions from an API (json server).
+  useEffect(function () {
+    async function fetchQuestions() {
+      try {
+        const response = await fetch("http://localhost:8000/questions");
+        const questions = await response.json();
+        dispatch({ type: "dataReceived", payload: questions });
+        // Debug
+        console.debug(questions);
+      } catch (fetchError) {
+        console.error(`Error fetching question API: ${fetchError.message}`);
+        dispatch({ type: "dataFetchError" });
+      }
+    }
+    fetchQuestions();
+  }, []);
+
+  // App's JSX structure.
   return (
     <div className="app">
       <Header />
